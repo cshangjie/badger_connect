c78ce0d572db5a6c30ac1a50e2e922abee561943
Christopher <cjsu@wisc.edu>
2023-04-05 16:34:08 -0500

    clearing gen info activity

--- app/src/main/java/com/example/badgerconnect/ProfileCreationActivity.java
+++ app/src/main/java/com/example/badgerconnect/ProfileCreationActivity.java
@@ -9,16 +9,25 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public class ProfileCreationActivity extends AppCompatActivity {
 
     private static final int MAX_COURSES = 6;
     private EditText nameTV, majorTV, coursesTV, meeting_typeTV;
-    private Button addCourseFieldButton, removeCourseFieldButton;
+    private Button addCourseFieldButton, removeCourseFieldButton, continueButton;
     private LinearLayout autocompleteContainer;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +35,14 @@ public class ProfileCreationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_creation);
 
         initializeUI();
+        mAuth = FirebaseAuth.getInstance();
 
         addCourseFieldButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (autocompleteContainer.getChildCount() < MAX_COURSES) {
                     AutoCompleteTextView autoCompleteTextView = new AutoCompleteTextView(ProfileCreationActivity.this);
+                    autoCompleteTextView.setHint("Enter Course Name");
                     autoCompleteTextView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                     String[] options = getResources().getStringArray(R.array.CourseList);
                     ArrayAdapter<String> adapter = new ArrayAdapter<>(ProfileCreationActivity.this, android.R.layout.simple_dropdown_item_1line, options);
@@ -61,6 +72,32 @@ public class ProfileCreationActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // On-click, need to verify that the user has at least entered in one course, and then continue
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Set<Courses> coursesSet = new HashSet<Courses>();
+                for (int i = 0; i < autocompleteContainer.getChildCount(); i++) {
+                    View child = autocompleteContainer.getChildAt(i);
+                    if (child instanceof AutoCompleteTextView) {
+                        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) child;
+                        String enteredText = autoCompleteTextView.getText().toString();
+                        if (!enteredText.isEmpty()) {
+                            coursesSet.add(Courses.valueOf(enteredText));
+                        }
+                    }
+                }
+                if (coursesSet.size() < 1){
+                    Toast.makeText(getApplicationContext(), "Please enter at least one course.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // create a list from the set and then update the user
+                List<Courses> userCourses = new ArrayList<>(coursesSet);
+
+            }
+        });
     }
 
     private void initializeUI() {
@@ -72,9 +109,11 @@ public class ProfileCreationActivity extends AppCompatActivity {
         removeCourseFieldButton = findViewById(R.id.removeButton);
         removeCourseFieldButton.setEnabled(false);
         autocompleteContainer = findViewById(R.id.autocomplete_container);
+        continueButton = findViewById(R.id.continueButton);
 
         // make the starting course entry box
         AutoCompleteTextView autoCompleteTextView = new AutoCompleteTextView(ProfileCreationActivity.this);
+        autoCompleteTextView.setHint("Enter Course Name");
         autoCompleteTextView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         String[] options = getResources().getStringArray(R.array.CourseList);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(ProfileCreationActivity.this, android.R.layout.simple_dropdown_item_1line, options);
--- app/src/main/java/com/example/badgerconnect/ProfileCreationGeneraInfoActivity.java
+++ app/src/main/java/com/example/badgerconnect/ProfileCreationGeneraInfoActivity.java
@@ -1,41 +1,24 @@
 package com.example.badgerconnect;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
-import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfileCreationGeneraInfoActivity {
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+public class ProfileCreationGeneraInfoActivity extends AppCompatActivity {
+    // declarations
+    private EditText nameField;
+    private AutoCompleteTextView majorField;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_creation_general_info);
 
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(requireContext(), this, year, month, day);
-        }
+    }
 
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-        }
+    private void initializeUI() {
 
-        public void showDatePickerDialog(View v) {
-            DialogFragment newFragment = new DatePickerFragment();
-            newFragment.show(getSupportFragmentManager(), "datePicker");
-        }
-
-        private FragmentManager getSupportFragmentManager() {
-        }
     }
 }
