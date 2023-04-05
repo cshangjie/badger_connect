6b360b3ede7864b914da27f75e8e6b35693b3a71
SahasG <gelli@wisc.edu>
2023-04-05 16:43:05 -0500

    EAC-23--profilepicture: Added profile picture selection

--- app/src/main/java/com/example/badgerconnect/ProfileCreationActivity.java
+++ app/src/main/java/com/example/badgerconnect/ProfileCreationActivity.java
@@ -1,27 +1,120 @@
 package com.example.badgerconnect;
 
-import android.os.Bundle;
-import android.text.Editable;
-import android.widget.EditText;
+import static com.example.badgerconnect.DatabaseFunctions.uploadPFP;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
 
 public class ProfileCreationActivity extends AppCompatActivity {
 
-    private EditText nameTV, majorTV, coursesTV, meeting_typeTV;
+    private ImageView set;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_profilepic);
 
         initializeUI();
     }
 
     private void initializeUI() {
-        nameTV = findViewById(R.id.name);
-        majorTV = findViewById(R.id.major);
-        coursesTV = findViewById(R.id.courses);
-        meeting_typeTV = findViewById(R.id.meeting_type);
+        set = findViewById(R.id.setting_profile_image);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        firebaseUser = firebaseAuth.getCurrentUser();
     }
+
+    public void updatePic(View v) {
+        requestExternalStoragePermission();
+    }
+
+    private void requestExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            // Permission has already been granted, access external storage here
+            imageChooser();
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
+            // If the request is cancelled, the grantResults array is empty
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted, access external storage here
+                imageChooser();
+            } else {
+                // Permission has been denied, show a message to the user
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                            set.setImageBitmap(
+                                    selectedImageBitmap);
+                            uploadPFP("00001", selectedImageBitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 }
