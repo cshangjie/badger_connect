4fa4cff14b114954be7e5e8f990b1eb0789e565f
Che-cyber <65218801+Che-cyber@users.noreply.github.com>
2023-04-26 16:47:51 -0500

    CJ's changes to Divy's branch

---
+++ app/src/main/java/com/example/badgerconnect/Fragments/MapsFragment.java
@@ -0,0 +1,78 @@
+package com.example.badgerconnect.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.badgerconnect.R;
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MapsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+
+    /////////////ADDED FROM MAPS ACTIVITY///////////////////////
+    private GoogleMap mMap;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+    private final long MIN_TIME = 1000;
+    private final long MIN_DIST = 5;
+
+    private Marker previousMarker;
+    private Location previousLocation;
+    private EditText editTextLatitude;
+    private EditText editTextLongitude;
+
+
+    ////////////////////////////////////////////////////////////
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        return view;
+    }
+}
